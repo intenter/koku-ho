214daa633695e12fca302fd546c0aa4889681589
intenter <int@intenter.org>
2017-06-16 23:30:43 -0500

    Added observable timer

--- src/pages/progress/progress.ts
+++ src/pages/progress/progress.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {Observable} from 'rxjs/Rx';
+import {Subscription} from "rxjs";
+
 
 @Component({
   selector: 'page-progress',
@@ -7,13 +10,31 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ProgressPage {
   interval: number;
+  counting: boolean;
+  subscription: Subscription;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.counting = true;
     this.interval = navParams.get('interval');
+    const minute = 1000;
+    let timer = Observable.timer(minute, minute);
+    this.subscription = timer.subscribe( t => {
+      this.interval--;
+      if (this.interval === 0) {
+        this.subscription.unsubscribe();
+        console.log('Done!');
+      }
+    });
+  }
+
+  pauseCountDown() {
+    console.log(`Current interval: ${this.interval}`);
   }
 
   stopCountDown() {
-    console.log(this.interval);
-    console.log(`Counting ${this.interval} minutes`);
+    this.subscription.unsubscribe();
+    this.navCtrl.pop();
   }
+
 
 }
