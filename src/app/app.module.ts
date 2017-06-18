dd93f19b75682ff55d54cbb66c59c4bce684bb1a
intenter <int@intenter.org>
2017-06-18 16:02:07 -0500

    Added audio plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { NativeAudio } from '@ionic-native/native-audio';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -29,6 +31,7 @@ import { CeilPipe } from './ceil.pipe';
   providers: [
     StatusBar,
     SplashScreen,
+    NativeAudio,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/progress/progress.ts
+++ src/pages/progress/progress.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {Observable} from 'rxjs/Rx';
 import {Subscription} from "rxjs";
-//import { NativeAudio } from '@ionic-native/native-audio';
+import { NativeAudio } from '@ionic-native/native-audio';
 
 
 @Component({
@@ -14,7 +14,13 @@ export class ProgressPage {
   counting: boolean;
   subscription: Subscription;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private nativeAudio: NativeAudio) {
+    this.nativeAudio.preloadSimple('dorbell', 'audio/dorbell.mp3')
+    .then(() => {
+      console.log('Sound loaded')
+    }, err => {
+      console.log(err);
+    })
     this.counting = true;
     this.interval = navParams.get('interval');
     const oneSecond = 1000;
@@ -26,13 +32,18 @@ export class ProgressPage {
       this.interval--;
       if (this.interval === 0) {
         this.subscription.unsubscribe();
-        this.navCtrl.pop();
+        this.nativeAudio.play('dorbell').then(()=>{
+          //this.navCtrl.pop();
+        });        
       }
     });
   }
 
   pauseCountDown() {
     this.counting = !this.counting;
+    this.nativeAudio.play('dorbell').then(()=>{
+          console.log('Bing');
+        });        
   }
 
   stopCountDown() {
