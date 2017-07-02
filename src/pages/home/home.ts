bea60523d43211e8a858d78287e9df9ee8196008
intenter <int@intenter.org>
2017-07-02 18:47:30 -0500

    Added ability to select sounds and how many times to play when finished

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,22 +2,42 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ProgressPage } from '../progress/progress'
 
+export class SoundType {
+  constructor(public readonly name: string, public readonly url: string) {
+  }
+}
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
   interval: string;
+  endWithCount: number;
+  endWithSoundType: SoundType;
+
+  sounds: SoundType[] = [
+    new SoundType('Door Bell', 'audio/dorbell.mp3'), 
+    new SoundType('Gong', 'audio/gong.mp3')];
+
   constructor(public navCtrl: NavController) {
     this.interval = '00:00';
+    this.endWithCount = 1;
+    this.endWithSoundType = this.sounds[0];
   }
 
   startCountDown() {
-    console.log(this.interval);
     const [hrs, mins] = this.interval.split(':');
     const intervalInSec: number = 60*(+hrs*60 + (+mins));
     console.log(`Counting ${intervalInSec} seconds`);
-    this.navCtrl.push(ProgressPage, {interval: intervalInSec});
+    console.log(`Selected sound: ${JSON.stringify(this.endWithSoundType)}`);
+    let options = {
+      interval: intervalInSec,
+      endWithCount: this.endWithCount,
+      endWithSoundType: this.endWithSoundType
+    };
+    this.navCtrl.push(ProgressPage, options);
   }
 
 }
--- src/pages/progress/progress.ts
+++ src/pages/progress/progress.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {Observable} from 'rxjs/Rx';
-import {Subscription} from "rxjs";
+import {Subscription} from 'rxjs';
 import { NativeAudio } from '@ionic-native/native-audio';
+import { SoundType } from '../home/home';
 
 
 @Component({
@@ -15,7 +16,8 @@ export class ProgressPage {
   subscription: Subscription;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private nativeAudio: NativeAudio) {
-    this.nativeAudio.preloadSimple('dorbell', 'audio/dorbell.mp3')
+    const endWithSoundType = navParams.get('endWithSoundType');
+    this.nativeAudio.preloadSimple(endWithSoundType.name, endWithSoundType.url)
     .then(() => {
       console.log('Sound loaded')
     }, err => {
@@ -32,18 +34,52 @@ export class ProgressPage {
       this.interval--;
       if (this.interval === 0) {
         this.subscription.unsubscribe();
-        this.nativeAudio.play('dorbell').then(()=>{
+        this.playSounds(endWithSoundType, navParams.get('endWithCount')).then(()=>{
           //this.navCtrl.pop();
         });        
       }
     });
   }
 
+  /**
+   * Repeats promise several times running them in sequence
+   * @param promiseFactory function that returns a promise
+   * @param {number} count how many times to repeat the promise
+   */
+  repeatPromise(promiseFactory, count) {
+    if (count > 0) {
+        return promiseFactory().then(() => this.repeatPromise(promiseFactory, count - 1));
+    } else {
+        return Promise.resolve();
+    }
+  }
+
+  /**
+   * Playes the sound of the given ID. The promise will be resolved when
+   * then sound stops playing
+   * @param {string} id ID of the preloaded sound
+   */
+  playSoundAndWhenDone(id: string) {
+    return new Promise((resolve, reject) => {
+      this.nativeAudio.play(id, () => {
+        resolve();
+      }).catch(reject)
+    });
+  }
+
+  /**
+   * Plase particular sound given number of times
+   * @param {SoundType} type type of sound
+   * @param {number} count how many times to play
+   */
+  playSounds(type: SoundType, count: number) {
+    return this.repeatPromise(() => {
+      return this.playSoundAndWhenDone(type.name);
+    }, count);
+  }
+
   pauseCountDown() {
     this.counting = !this.counting;
-    this.nativeAudio.play('dorbell').then(()=>{
-          console.log('Bing');
-        });        
   }
 
   stopCountDown() {
