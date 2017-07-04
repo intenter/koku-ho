fa6da16e029d3e0d81ea123652c7d72ba04af5cd
intenter <int@intenter.org>
2017-07-04 15:53:37 -0500

    Added lodash. Added lines per additional sounds and binding

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ProgressPage } from '../progress/progress'
+import * as _ from 'lodash'
 
 export class SoundType {
   constructor(public readonly name: string, public readonly url: string) {
   }
 }
 
+export class SoundSignal {
+  constructor(
+    public delay: number, 
+    public times: number,
+    public soundType: SoundType) {}
+}
 
 @Component({
   selector: 'page-home',
@@ -21,10 +28,22 @@ export class HomePage {
     new SoundType('Door Bell', 'audio/dorbell.mp3'), 
     new SoundType('Gong', 'audio/gong.mp3')];
 
+  additionalSignals: SoundSignal[] = [new SoundSignal(1, 3, this.sounds[0])];
+
   constructor(public navCtrl: NavController) {
     this.interval = '00:00';
     this.endWithCount = 1;
     this.endWithSoundType = this.sounds[0];
+  }
+
+  addSignal() {
+    this.additionalSignals.push(new SoundSignal(0, 3, this.sounds[1]));
+  }
+
+  deleteSignal(signal: SoundSignal) {
+    _.remove(this.additionalSignals, (item) => {
+      return item === signal
+    });
   }
 
   startCountDown() {
